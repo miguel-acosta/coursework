65dbf6abc2559d4ef5763a4c50a085d91b45eff1
Miguel Acosta <jmacostaa@gmail.com>
2017-09-13 12:55:34 -0400

    MacroMetrics ps1.1

--- ng/ps1/ps1.jl
+++ ng/ps1/ps1.jl
@@ -3,7 +3,7 @@ using Distributions
 using PyPlot
 srand(6413)
 #------------------------------------------------------------------------------#
-#----------------------------------- Q1 ---------------------------------------#
+#----------------------------------- Q1.a -------------------------------------#
 #------------------------------------------------------------------------------#
 
 
@@ -28,12 +28,14 @@ jb = (N/6)*(sk^2 + (1/4)*(ku-3)^2)
 
 # qq-plot time...
 quants  = collect(0.01:0.01:0.99)
-normQuants = quantile(Normal(mean(x),var(x)), quants) # should this be normal 0,1?? 
+normQuants = quantile(Normal(mean(x),sqrt(var(x))), quants)
 lambQuants = quantile(vec(x),quants)
 
 ioff()
 f = figure()
-plot([0,maximum(normQuants)],[0,maximum(normQuants)], color = "black",label = "45 Deg. Line")
+ymin = minimum([minimum(normQuants), minimum(lambQuants)])
+ymax = maximum([maximum(normQuants), maximum(lambQuants)])
+plot([ymin,ymax],[ymin,ymax], color = "black",label = "45 Deg. Line")
 scatter(normQuants, lambQuants, color="red", label = "Quantiles")
 xlabel("Normal")
 ylabel("Generalized Lambda")
@@ -41,3 +43,15 @@ title("QQ-Plot of Generalized Lambda and Normal Distributions")
 legend()
 
 savefig("qq.pdf")
+
+
+#------------------------------------------------------------------------------#
+#----------------------------------- Q1.d -------------------------------------#
+#------------------------------------------------------------------------------#
+include("newtonRaphson.jl")
+g(μ,σ2) = 1/N * [sum(x-μ), sum((x-μ).^2 - σ2),sum((x-μ).^3),
+                     sum((x-μ).^4 - 3 * σ2^2)]
+
+J(theta) = g(theta[1],theta[2]).' * g(theta[1],theta[2])
+
+θ,exitStatus = newtonRaphson(J, [0.02,0.001],1e-7,1e5)
