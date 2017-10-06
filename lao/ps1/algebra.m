0c44ba0d8c4beae3e1d76064db4041b7cb4206d6
Miguel Acosta <jmacostaa@gmail.com>
2017-10-06 15:45:17 -0400

    More more macro ps1q4

--- lao/ps1/algebra.m
+++ lao/ps1/algebra.m
@@ -1,3 +1,11 @@
-syms phix beta kx kp p theta
+clear all
+syms phix phip beta kx kp p theta
 
-phix = (1-beta + beta * phix)*(kx/(kx+kp))
+PHIX = solve(phix == (1-beta + beta * phix)*(kx/(kx+kp)));
+
+PHIP = solve(phip == (1-beta-beta*PHIX)*kp/(kx+kp) + beta * phip);
+
+R    = (1-beta)*theta + beta*(PHIX*theta + PHIP*p);
+
+deltap = (kx +kp)/((1-beta+beta*PHIX)^2) - kp/(1-beta+beta*PHIX);
+deltax = -kx/(1-beta + beta * PHIX);
