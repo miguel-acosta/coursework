de04daf5b5bdf6a16c4cdf506507322148f557b1
Miguel Acosta <jmacostaa@gmail.com>
2017-10-06 15:52:02 -0400

    More more macro ps1q4 with risk aversion

--- lao/ps1/algebra.m
+++ lao/ps1/algebra.m
@@ -1,5 +1,5 @@
 clear all
-syms phix phip beta kx kp p theta
+syms phix phip beta kx kp p theta gamma
 
 PHIX = solve(phix == (1-beta + beta * phix)*(kx/(kx+kp)));
 
@@ -7,5 +7,5 @@ PHIP = solve(phip == (1-beta-beta*PHIX)*kp/(kx+kp) + beta * phip);
 
 R    = (1-beta)*theta + beta*(PHIX*theta + PHIP*p);
 
-deltap = (kx +kp)/((1-beta+beta*PHIX)^2) - kp/(1-beta+beta*PHIX);
-deltax = -kx/(1-beta + beta * PHIX);
+deltap = (kx +kp)/(gamma*(1-beta+beta*PHIX)^2) - kp/(gamma*(1-beta+beta*PHIX));
+deltax = -kx/(gamma*(1-beta + beta * PHIX));
